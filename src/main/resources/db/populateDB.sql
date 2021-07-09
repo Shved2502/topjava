35370c6a7eb7619d7c72dac5db51a1af733fec90
shved <vad.shvedov@mail.ru>
2021-07-09 15:17:33 +0300

    HW3 add and init DB

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,12 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES (date_trunc('minute', TIMESTAMP'2020-01-30 10:00:10'), 'Завтрак', 500, 100000),
+       (date_trunc('minute', TIMESTAMP'2020-01-30 13:00:10'), 'Обед', 1000, 100000),
+       (date_trunc('minute', TIMESTAMP'2020-01-30 20:00:10'), 'Ужин', 500, 100000),
+       (date_trunc('minute', TIMESTAMP'2020-01-31 00:00:10'), 'Граничное значение', 100, 100000),
+       (date_trunc('minute', TIMESTAMP'2020-01-31 10:00:10'), 'Завтрак', 1000, 100000),
+       (date_trunc('minute', TIMESTAMP'2020-01-31 13:00:10'), 'Обед', 500, 100000),
+       (date_trunc('minute', TIMESTAMP'2020-01-31 20:00:10'), 'Ужин', 410, 100000);
